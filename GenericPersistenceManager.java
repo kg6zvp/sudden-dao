306d1988163a40f5141bcf826effd5c1679e5bbb
kg6zvp <kg6zvp@users.noreply.github.com>
2016-06-07 22:50:01 -0700

    Add files via upload

---
+++ GenericPersistenceManager.java
@@ -0,0 +1,191 @@
+package enterprises.mccollum.utils.genericentityejb;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.Id;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ * Intended to be subclassed for use as a persistence manager. It contains all boilerplate code for a class used to manage persistent Entities.
+ * 
+ * Usage:
+ *	Extend this persistence manager class and annotate your bean as {@link Local} and {@link Stateless}
+ *	Create a default constructor with a single call to super(Class.class); where Class is the name of the {@link Entity} you wish to manage
+ * @author Sam McCollum
+ * 
+ * @param <T> The type of Object being managed
+ * @param <K> The type used for the Entity's {@link Id}
+ */
+public class GenericPersistenceManager<T, K> {
+	Class<T> cArg;
+	String tableName;
+	
+	@PersistenceContext
+	EntityManager em;
+	
+	public GenericPersistenceManager(Class<T> cArg, String tableName){
+		this.cArg = cArg;
+		this.tableName = tableName;
+	}
+	
+	public GenericPersistenceManager(Class<T> cArg){
+		this(cArg, cArg.getSimpleName());
+	}
+	
+	/**
+	 * Check whether or not an object with the given key (id) is in the database
+	 * @param key
+	 * @return
+	 */
+	public boolean containsKey(K key){
+		if(key == null)
+			return false;
+		return (get(key) != null);
+	}
+	
+	/**
+	 * Persist and return the object
+	 * @param data
+	 * @return
+	 */
+	public T persist(T data){
+		em.persist(data);
+		em.flush();
+		return data;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public K getId(T data) {
+		Field[] members = cArg.getDeclaredFields();
+		for(Field member : members){
+			try {
+				//System.out.printf("Processing field: %s\n", member.getName());
+				member.setAccessible(true);
+				if(member.getDeclaredAnnotationsByType(Id.class).length != 0){
+					return (K)member.get(data);
+				}
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Save an object to the database
+	 * @param data
+	 * @return
+	 */
+	public T save(T data){
+		return em.merge(data);
+	}
+	/**
+	 * Retrieve an object from the database with the given key (id)
+	 * @param key
+	 * @return
+	 */
+	public T get(K key){
+		return em.find(cArg, key);
+	}
+	/**
+	 * Remove an object from the database
+	 * @param data
+	 */
+	public void remove(T data){
+		em.remove(em.merge(data));
+	}
+	/**
+	 * Save a list of objects to the database
+	 * @param data
+	 */
+	public void saveAll(List<T> data){
+		for(T d : data)
+			save(d);
+	}
+	/**
+	 * Remove a list of objects from the database
+	 * @param data
+	 */
+	public void removeAll(List<T> data){
+		for(T d : data)
+			remove(d);
+	}
+	/**
+	 * Retrieve a list of {@link T}, all of the ones in the database
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<T> getAll(){
+		Query q = getSelectAllQuery();
+		return (List<T>)q.getResultList();
+	}
+	
+	/**
+	 * Checks whether or not the table is empty
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public boolean isTableEmpty(){
+		List<T> results = getSelectAllQuery().setMaxResults(2).getResultList();
+		return (results.size() < 1);
+	}
+	/**
+	 * private helper method
+	 * @return
+	 */
+	private Query getSelectAllQuery(){
+		return em.createQuery(getSelectAllQueryString());
+	}
+	/**
+	 * private helper method
+	 * @return
+	 */
+	private String getSelectAllQueryString(){
+		return "SELECT data FROM "+tableName+" data";
+	}
+	
+	/**
+	 * Retrieve a class from the database whose non-null instance variables match those of the class in the database
+	 * @param keyObject
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public List<T> getMatching(T keyObject){
+		Field[] members = cArg.getDeclaredFields();
+		Map<String, Object> importantFields = new TreeMap<>();
+		for(Field member : members){
+			try {
+				member.setAccessible(true);
+				if(!member.getType().isPrimitive()){
+					Object val = member.get(keyObject);
+					if(val != null)
+						importantFields.put(member.getName(), val);
+				}
+			} catch (SecurityException e) {
+				e.printStackTrace();
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+		String queryString = getSelectAllQueryString();
+		for(Map.Entry<String, Object> field : importantFields.entrySet()){
+			queryString += String.format(" where data.%s = :%s", field.getKey(), field.getKey());
+		}
+		System.out.printf("Composed query string:\n%s\n", queryString);
+		Query q = em.createQuery(queryString);
+		for(Map.Entry<String, Object> field : importantFields.entrySet()){
+			q.setParameter(field.getKey(), field.getValue());
+		}
+		return q.getResultList();
+	}
+}
